e3590abcc909f9f834081df59c0f9a6ceaddfa5b
Mayssam Alwaraa <mayssamalwaraa295@gmail.com>
2024-09-01 15:37:19 +0100

    Add NewsFeed component

---
+++ src/components/NewsFeed.jsx
@@ -0,0 +1,12 @@
+import NewsArticle from "./NewsArticle";
+
+function NewsFeed(props) {
+    const {articles}=props;
+    return (
+        <div>
+            {articles.map((article)=><NewsArticle key={JSON.stringify(article)} article={...article} />)}
+        </div>
+    )
+}
+
+export default NewsFeed
