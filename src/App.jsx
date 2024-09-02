5dfea4bb2a47a0b531744c2b8a22a93dc388fa8d
Mayssam Alwaraa <mayssamalwaraa295@gmail.com>
2024-09-02 21:15:03 +0100

    add api key to .env and fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
 import { Container } from "@mui/system";
+import { useState } from "react";
+import { useEffect } from "react";
 import NewsFeed from "./components/NewsFeed";
 import NewsHeader from "./components/NewsHeader";
-import { SAMPLE_ARTICLE } from "./constant/sampleArticle";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const loadArticles = async () => {
+    const response = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=eg&apiKey=${
+        import.meta.env.VITE_NEWS_FEED_API_KEY
+      }`
+    );
+    if (!response.ok) console.log("errrrrrrrrrrrrrrr");
+    const data = await response.json();
+    console.log(data);
+    return data.articles.map((article) => {
+      const { title, description, author, publishedAt, urlToImage } = article;
+      return {
+        title,
+        description,
+        author,
+        publishedAt,
+        image: urlToImage,
+      };
+    });
+  };
+  useEffect(() => {
+    loadArticles().then(setArticles);
+  }, []);
   return (
     <Container>
       <NewsHeader />
-      <NewsFeed articles={SAMPLE_ARTICLE} />
+      <NewsFeed articles={articles} />
     </Container>
   );
 }
